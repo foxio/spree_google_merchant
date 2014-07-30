661652e4446c40588d9029e11c56d8995d478183
Dries Steenhouwer <steenhouwer.dries@gmail.com>
2014-07-30 13:12:12 +0200

    add support for locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.routes.draw do
-  get "google_merchant" => 'google_merchant#index', :constraints => {:format => :rss}
+  match "google_merchant/:locale/feed" => 'google_merchant#index', :constraints => {:format => :rss}
 
   namespace :admin do
     resource :google_merchants
