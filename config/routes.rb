37370994116b4d3ca0fb9fd840beaf21154d05da
Bob Mattax <rlmattax@gmail.com>
2014-08-21 13:24:15 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Spree::Core::Engine.routes.append do
-  match "google_merchant", :to => 'products#google_merchant'
+  resources :products do 
+    get :google_merchant
+  end
 
   namespace :admin do
     resource :google_merchants
